ad4e2b8a54d0b4483da5c89e1e3d706f88f3545c
Jack Taylor <jack.taylor.2048@gmail.com>
2016-08-29 11:13:17 +0100

    Added attributes and method definitions

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,1 +1,45 @@
 #include "quadtree.h"
+Quadtree::Quadtree(int level, float position_x, float position_y, int width, int height)
+{
+
+}
+Quadtree::~Quadtree()
+{
+
+}
+void Quadtree::update()
+{
+
+}
+void Quadtree::collisions()
+{
+
+}
+void Quadtree::add()
+{
+
+}
+void Quadtree::partition()
+{
+
+}
+void Quadtree::cleanup()
+{
+
+}
+void Quadtree::split()
+{
+
+}
+void Quadtree::clear()
+{
+
+}
+bool Quadtree::contains()
+{
+
+}
+bool Quadtree::leaf()
+{
+
+}
--- src/quadtree.h
+++ src/quadtree.h
@@ -1,10 +1,34 @@
 #ifndef QUADTREE_H
 #define QUADTREE_H
+#include <vector>
+/*
+ * A generic Quadtree class. This will perform spatial partitioning on a vector of objects, 
+ * allowing for faster collision detection.
+ */
 class Quadtree
 {
 public:
-
-private:
-
+	Quadtree(int level, float position_x, float position_y, int width, int height);
+	~Quadtree();
+	void update();
+	void collisions();
+	void add();
+	void partition();
+	void cleanup();
+protected:
+	void split();
+	void clear();
+	bool contains();
+	bool leaf();
+	static const int MAX_LEVEL = 5;
+	unsigned int max_objects;
+	int level;
+	float position_x;
+	float position_y;
+	int width;
+	int height;
+	// int is a placeholder; this will be changed to a generic type
+	std::vector<int> objects;
+	Quadtree* child[4];
 };
 #endif
