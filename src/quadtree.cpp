b4d7f2d7b3a75d922ef61b0efa2fab84a7d9b3ff
Jack Taylor <jack.taylor.2048@gmail.com>
2016-08-29 11:21:14 +0100

    Implemented constructor
    Also commented attributes

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,7 +1,16 @@
 #include "quadtree.h"
+// Constructor
 Quadtree::Quadtree(int level, float position_x, float position_y, int width, int height)
 {
-
+	max_objects = 50;
+	if (level <= MAX_LEVEL) Quadtree::level = level;
+	else Quadtree::level = MAX_LEVEL;
+	Quadtree::position_x = position_x;
+	Quadtree::position_y = position_y;
+	Quadtree::width = width;
+	Quadtree::height = height;
+	for (int i = 0; i < 4; i++) child[i] = NULL;
+	objects.reserve(4096);
 }
 Quadtree::~Quadtree()
 {
--- src/quadtree.h
+++ src/quadtree.h
@@ -3,7 +3,8 @@
 #include <vector>
 /*
  * A generic Quadtree class. This will perform spatial partitioning on a vector of objects, 
- * allowing for faster collision detection.
+ * allowing for faster collision detection. Each instance of this class represents a node 
+ * of a quadtree, since nodes are quadtrees themselves.
  */
 class Quadtree
 {
@@ -20,15 +21,21 @@ protected:
 	void clear();
 	bool contains();
 	bool leaf();
+	// The maximum number of "levels", i.e. the amount of times the quadtree may split.
 	static const int MAX_LEVEL = 5;
+	// The maximum number of objects before the quadtree will split.
 	unsigned int max_objects;
+	// The current level of this particular quadtree node. The root is at level 0.
 	int level;
+	// The position of this node. The root should have a position of 0, 0.
 	float position_x;
 	float position_y;
+	// The size of this node. The root should have a size equal to that of the window.
 	int width;
 	int height;
 	// int is a placeholder; this will be changed to a generic type
 	std::vector<int> objects;
+	// Pointers to the children of this node.
 	Quadtree* child[4];
 };
 #endif
