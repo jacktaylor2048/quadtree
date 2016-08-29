53f7795a9d627c669f72da4ca7485b5868fb8ca2
Jack Taylor <jack.taylor.2048@gmail.com>
2016-08-29 15:42:28 +0100

    Modified Entity constructors

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,33 +1,18 @@
 #include "entity.h"
 // Default Constructor
-Entity::Entity()
+Entity::Entity() : position_x(0), position_y(0), boundary_x(0), boundary_y(0), offset_x(0), offset_y(0)
 {
-	position_x = 0;
-	position_y = 0;
-	boundary_x = 0;
-	boundary_y = 0;
-	offset_x = 0;
-	offset_y = 0;
+	
 }
 // Basic Constructor (excludes offsets)
-Entity::Entity(float px, float py, float bx, float by)
+Entity::Entity(float px, float py, float bx, float by) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(0), offset_y(0)
 {
-	position_x = px;
-	position_y = py;
-	boundary_x = bx;
-	boundary_y = by;
-	offset_x = 0;
-	offset_y = 0;
+	
 }
 // Full Constructor (includes offsets)
-Entity::Entity(float px, float py, float bx, float by, float ox, float oy)
+Entity::Entity(float px, float py, float bx, float by, float ox, float oy) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(ox), offset_y(oy)
 {
-	position_x = px;
-	position_y = py;
-	boundary_x = bx;
-	boundary_y = by;
-	offset_x = ox;
-	offset_y = oy;
+	
 }
 // Return true if this entity is colliding with other entity
 bool Entity::check_collision(Entity* ent)
