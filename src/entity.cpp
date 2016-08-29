42f9c8ac238c9b58f8b219937203b23cf003b2d2
Jack Taylor <jack.taylor.2048@gmail.com>
2016-08-29 11:32:33 +0100

    Implemented Entity constructors

--- src/entity.cpp
+++ src/entity.cpp
@@ -2,15 +2,30 @@
 // Default Constructor
 Entity::Entity()
 {
-
+	position_x = 0;
+	position_y = 0;
+	boundary_x = 0;
+	boundary_y = 0;
+	offset_x = 0;
+	offset_y = 0;
 }
 // Basic Constructor (excludes offsets)
 Entity::Entity(float px, float py, float bx, float by)
 {
-
+	position_x = px;
+	position_y = py;
+	boundary_x = bx;
+	boundary_y = by;
+	offset_x = 0;
+	offset_y = 0;
 }
 // Full Constructor (includes offsets)
 Entity::Entity(float px, float py, float bx, float by, float ox, float oy)
 {
-
+	position_x = px;
+	position_y = py;
+	boundary_x = bx;
+	boundary_y = by;
+	offset_x = ox;
+	offset_y = oy;
 }
