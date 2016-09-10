d6a07a4067f8f3398e4bbfb50508b5abee1673b3
Jack Taylor <jack.taylor.2048@gmail.com>
2016-09-10 12:33:41 +0100

    Renamed Entity to ExampleEntity

--- quadtree/src/example_entity.cpp
+++ quadtree/src/example_entity.cpp
@@ -1,31 +1,31 @@
-#include "entity.h"
+#include "example_entity.h"
 
 // Default Constructor
-Entity::Entity() : position_x(0), position_y(0), boundary_x(0), boundary_y(0), offset_x(0), offset_y(0)
+ExampleEntity::ExampleEntity() : position_x(0), position_y(0), boundary_x(0), boundary_y(0), offset_x(0), offset_y(0)
 {
 	
 }
 
 // Basic Constructor (excludes offsets)
-Entity::Entity(double px, double py, double bx, double by) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(0), offset_y(0)
+ExampleEntity::ExampleEntity(double px, double py, double bx, double by) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(0), offset_y(0)
 {
 	
 }
 
 // Full Constructor (includes offsets)
-Entity::Entity(double px, double py, double bx, double by, double ox, double oy) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(ox), offset_y(oy)
+ExampleEntity::ExampleEntity(double px, double py, double bx, double by, double ox, double oy) : position_x(px), position_y(py), boundary_x(bx), boundary_y(by), offset_x(ox), offset_y(oy)
 {
 	
 }
 
 // Virtual method; sub-classes would define update behaviour per frame here.
-void Entity::update()
+void ExampleEntity::update()
 {
 
 }
 
 // Return true if this entity is colliding with other entity
-bool Entity::check_collision(Entity* ent)
+bool ExampleEntity::check_collision(ExampleEntity* ent)
 {
 	return position_x + boundary_x + offset_x > ent->get_position_x() + ent->get_offset_x()
 		&& position_x + offset_x < ent->get_position_x() + ent->get_boundary_x() + ent->get_offset_x()
@@ -34,37 +34,37 @@ bool Entity::check_collision(Entity* ent)
 }
 
 // Virtual method; sub classes would define collision behaviour here.
-void Entity::collide(Entity* ent)
+void ExampleEntity::collide(ExampleEntity* ent)
 {
 
 }
 
-double Entity::get_position_x()
+double ExampleEntity::get_position_x()
 {
 	return position_x;
 }
 
-double Entity::get_position_y()
+double ExampleEntity::get_position_y()
 {
 	return position_y;
 }
 
-double Entity::get_boundary_x()
+double ExampleEntity::get_boundary_x()
 {
 	return boundary_x;
 }
 
-double Entity::get_boundary_y()
+double ExampleEntity::get_boundary_y()
 {
 	return boundary_y;
 }
 
-double Entity::get_offset_x()
+double ExampleEntity::get_offset_x()
 {
 	return offset_x;
 }
 
-double Entity::get_offset_y()
+double ExampleEntity::get_offset_y()
 {
 	return offset_y;
 }
